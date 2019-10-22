65af9e03b046c731b16189ec41d74b937d6522ee
Naomi Slater <nslater@tumbolia.org>
2019-10-22 15:02:28 +0200

    Add doc utils

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,17 @@
 from crate.theme.rtd.conf.sql_99 import *
 
-exclude_patterns = ["out/**"]
-
 # crate.theme sets html_favicon to favicon.png which causes a warning because
 # it should be a .ico and in addition there is no favicon.png in this project
 # so it can't find the file
 html_favicon = None
 
+source_suffix = '.rst'
+
+exclude_patterns = ['.*', '*.lint']
+
+master_doc = 'index'
+
 site_url = 'https://crate.io/docs/sql-99/en/latest/'
+
 extensions = ['sphinx_sitemap']
+
