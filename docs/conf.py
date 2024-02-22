4c191acc6417e90427756253cf6c1ab628975581
Matthias <matthias@crate.io>
2024-02-22 17:18:13 +0100

    add ``html_theme_options`` overrides directly in the sql-99 repo

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,9 @@
 from crate.theme.rtd.conf.sql_99 import *
 
+html_theme_options.update({
+    "canonical_url": "https://sql-99.readthedocs.io",
+})
+
 # Disable version chooser.
 html_context.update({
     "display_version": False,
