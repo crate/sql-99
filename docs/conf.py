3b5fe1154a387c04718d76ccbc7fc6117f34b8b9
Brian Munkholm <bmunkholm@gmail.com>
2025-06-27 14:13:48 +0200

    Update deprecated option html_theme_options

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,6 @@
 from crate.theme.rtd.conf.sql_99 import *
 
-html_theme_options.update({
-    "canonical_url": "https://sql-99.readthedocs.io",
-})
+html_baseurl = "https://sql-99.readthedocs.io/"
 
 # Disable version chooser.
 html_context.update({
